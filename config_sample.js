dc28e64f276eacd541652dc2b05974a8c5a681dc
niltea <futurabodoni@gmail.com>
2019-04-10 20:14:03 +0900

    configを別ファイルにした。

---
+++ config_sample.js
@@ -0,0 +1,13 @@
+// 元データとなる json ファイルを指定
+var filepath = 'hoge.json';
+// サークルカット格納パス
+var cutFilePath = 'circlecut/';
+
+// ページタイトル(prefix)
+var pageTitlePrefix = 'サークル一覧 / ';
+// ページタイトル(suffix)
+var pageTitleSuffix = '';
+// ページごとのサークル割当数
+var circlesInPage = 6;
+// circleブロックグループのprefix名
+var circleBlockPrefix = 'circleGroup-';
--- script.jsx
+++ script.jsx
@@ -1,15 +1,4 @@
-// 元データとなる json ファイルを指定
-var filepath = "/Users/niltea/Desktop/nijisanji02.json";
-// サークルカット格納パス
-var cutFilePath = '/Users/niltea/Desktop/nijisanji02_cut/';
-// ページタイトル(prefix)
-var pageTitlePrefix = 'サークル一覧 / ';
-// ページタイトル(suffix)
-var pageTitleSuffix = '';
-// ページごとのサークル割当数
-var circlesInPage = 6;
-// circleブロックグループのprefix名
-var circleBlockPrefix = 'circleGroup-';
+//@include "config.js";
 
 // Node.js環境かどうか調べる
 var isNode = (typeof process !== "undefined" && typeof require !== "undefined");
